11f31e6f5edfab01354f4d0794a8e42ac597db81
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-20 19:37:03 -0400

    unknow types
    start the lession
    
    * going through the parts of this type

---
+++ function.ts
@@ -0,0 +1,12 @@
+function sum(
+    n1: number,
+    n2: number,) {
+        return n1 + n2;
+    }
+
+
+    function printResults( num: number) {
+        console.log('print:' + num);
+    }
+
+    printResults(sum(15, 50));
