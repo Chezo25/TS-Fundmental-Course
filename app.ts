815941d1fa0106c86f63c3ccc6c804b6f954f6b8
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-19 20:37:26 -0400

    Update app.ts
    made changes

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function add(n1: number,n2: number,n3: number) {
+function add({ n1, n2, n3 }: { n1: number; n2: number; n3: number; }) {
     return n1 + n2 + n3;
 }
 
@@ -6,4 +6,4 @@ const number1 = 10;
 let number2 = 25.5;
 const number3 = 7;
 
-const answer = add(number1, number2, number3);
+const answer = add({ n1: number1, n2: number2, n3: number3 });
